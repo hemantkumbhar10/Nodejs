ed782b888d5b784db01c41f3fba4922f4e746075
Hemant Kumbhar <64783961+hemantkumbhar10@users.noreply.github.com>
2022-09-14 05:13:31 +0530

    Add files via upload

---
+++ CrudAPINodeJS/data.js
@@ -0,0 +1,35 @@
+const todos = [
+	{
+		id          : '1',
+		title       : 'Java',
+		description :
+			'Java is a Programming Language.'
+			
+	},
+	{
+		id          : '2',
+		title       : 'Node',
+		description :
+			'Node Js is a server side backend language.'
+	},
+	{
+		id          : '3',
+		title       : 'React JS',
+		description :
+			'React is a Library to create Front End UI.'
+	},
+	{
+		id          : '4',
+		title       : 'Dev Ops',
+		description :
+			'Dev + Operation and CI Deployment.'
+	},
+	{
+		id          : '5',
+		title       : 'Node and Dev Ops',
+		description :
+			'Node Backend along Dev + Operation and CI Deployment.'
+	}
+];
+
+module.exports = todos;
